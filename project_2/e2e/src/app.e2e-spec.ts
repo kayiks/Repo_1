91732d3053d52b7b6b1d466cfa44833e39bcb761
Nico <potencia2001@gmail.com>
2019-03-16 22:35:58 -0300

    prueba
    probando

--- project_2/e2e/src/app.e2e-spec.ts
+++ project_2/e2e/src/app.e2e-spec.ts
@@ -4,9 +4,10 @@ describe('new App', () => {
   let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new AppPage(); 
   });
 
+  
   it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getPageTitle()).toContain('Tab One');
